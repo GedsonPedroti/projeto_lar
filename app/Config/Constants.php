76c37982edf1944319e0f031220768c34294dea0
GedsonPedroti <gedson.pedroti@gmail.com>
2024-10-08 14:14:27 -0300

    001

--- app/Config/Constants.php
+++ app/Config/Constants.php
@@ -92,3 +92,8 @@ define('EVENT_PRIORITY_NORMAL', 100);
  * @deprecated Use \CodeIgniter\Events\Events::PRIORITY_HIGH instead.
  */
 define('EVENT_PRIORITY_HIGH', 10);
+
+/**
+ *  Definição das minhas constantes
+ */
+define('APP_NAME',  'Sistema Controle Lar');
